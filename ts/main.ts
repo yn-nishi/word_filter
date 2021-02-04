490ad884e82ecef0339f128246b96f2162974e46
Yuichi Nishi <yn@ynyn.mixh.jp>
2021-02-05 04:30:49 +0900

    change data structure

--- ts/main.ts
+++ ts/main.ts
@@ -1,14 +1,15 @@
 // Copyright 2021 yn-nishi All Rights Reserved.
 document.addEventListener("DOMContentLoaded",() => {
+  // chrome.storage.local.clear()
+
   chrome.storage.local.get(null, (storage)=>{
 
     // メイン処理
     const changeKeyword = (node: HTMLElement | ChildNode) => {
-      console.log('動作2')
       const type = node.nodeType
       if(type === 3 && !isIgnore(node)) {
-        Object.keys(storage).forEach( key => {
-          node.nodeValue = (<string>node.nodeValue).split(key).join(storage[key]);
+        Object.keys(storage.kw).forEach( key => {
+          node.nodeValue = (<string>node.nodeValue).split(key).join(storage.kw[key]);
         })
       } else if (type === 1 || type === 9 || type === 11) {
         let child: ChildNode | null = node.firstChild;
@@ -21,13 +22,10 @@ document.addEventListener("DOMContentLoaded",() => {
 
     // 設定読み込み
     const loadSettings = () => {
-      if(Object.keys(storage).length < 2) {
+      if(Object.keys(storage.kw).length < 2) {
         chrome.storage.local.set(initialSettings)
-        console.log(initialSettings)
       }
-      if(storage.extensionFunction === undefined) {
-        chrome.storage.local.set({'extensionFunction': true});
-      }
+      console.log('設定読み込み',storage)
     }
 
     // 処理しないタグ
@@ -44,10 +42,8 @@ document.addEventListener("DOMContentLoaded",() => {
         setTimeout(() => { resolve(null)}, 1500 )
       })
       .then(()=>{ 
-        // if((<HTMLElement>document.activeElement).isContentEditable === false){
-          if((<HTMLElement>document.activeElement).isContentEditable === false && storage.extensionFunction){
-          console.log('動作1-2')
-          console.log(storage)
+          if((<HTMLElement>document.activeElement).isContentEditable === false && storage.affect){
+          console.log('監視の反応あり')
           // changeKeyword(document.body)
         }
       })
@@ -61,14 +57,19 @@ document.addEventListener("DOMContentLoaded",() => {
 
     // メイン処理実行命令
     loadSettings()
-    if(storage.extensionFunction) {
-      // changeKeyword(document.body)
-      console.log('動作1-1')
-      console.log(storage)
+    if(storage.affect) {
+      changeKeyword(document.body)
+      console.log('メイン処理実行命令')
     }
     observer.observe(document.body, config)
   })
 
-  const initialSettings = { 'コロナ': 'コーラ', 'マスク': 'フリスク' }   
+  const initialSettings = {
+    affect: true,
+    kw: {
+      'コロナ': 'コーラ',
+      'マスク': 'フリスク'
+    }
+  }
 })
 
--- ts/popup.ts
+++ ts/popup.ts
@@ -3,18 +3,16 @@
 // 設定内容表示(メイン処理)
 chrome.storage.local.get(null, (storage)=>{
   loadSettings(storage)
-  // const $switch = document.getElementById('customSwitch1');
-  // $switch?.setAttribute('checked', storage['extensionFunction'])
-  const $inputArea = <HTMLElement>document.getElementById('input-area')
-  Object.keys(storage).forEach((k, i) => {
+  const $inputArea = document.getElementById('input-area') as HTMLElement
+  Object.keys(storage.kw).forEach((k, i) => {
     if(k !== 'extensionFunction') {
-      addInput($inputArea, i, k, storage[k])
+      addInput($inputArea, i, k, storage.kw[k])
     }
   })
-  addInput($inputArea, Object.keys(storage).length)
+  addInput($inputArea, Object.keys(storage.kw).length)
   document.addEventListener("change", (e) => {
       const numberOfInputs: number = document.querySelectorAll('#input-area input').length / 2
-      // console.log(numberOfInputs,'行')
+      console.log('全部で',numberOfInputs,'行あるよ')
       const selectedId = (<HTMLInputElement>e.target)?.getAttribute('id')
       if(selectedId === 'k-'+ (numberOfInputs - 1)) {
       addInput($inputArea, numberOfInputs)
@@ -24,38 +22,42 @@ chrome.storage.local.get(null, (storage)=>{
 
 // 設定初期化
 const loadSettings = (storage:{ [key: string]: string|boolean }) => {
-  if(Object.keys(storage).length < 2) {
+  console.log('読み込み一発目の現在の設定')
+  console.log(storage)
+  if(Object.keys(storage.kw).length < 2) {
     chrome.storage.local.set(initialSettings)
-    console.log(initialSettings,'が設定されました')
+    console.log(initialSettings,'初期設定が設定されました')
+    console.log(storage)
   }
   const $switch = document.getElementById('customSwitch1')
-  if(storage.extensionFunction === undefined) {
-    (<HTMLInputElement>$switch).checked = true
-    storage.extensionFunction = true
-    chrome.storage.local.set({'extensionFunction': true})
-    console.log('extensionFunctionが未設定だったのでtrueに設定されました')
-  }
   $switch?.addEventListener('change', () => {
-    storage.extensionFunction = (<HTMLInputElement>$switch).checked
-    chrome.storage.local.set({ 'extensionFunction':  storage.extensionFunction})
-    console.log('extensionFunctionの設定が',storage.extensionFunction,'に変更されました')
+    storage.affect = (<HTMLInputElement>$switch).checked
+    chrome.storage.local.set({ 'affect':  storage.affect})
+    console.log('affectの設定が',storage.affect,'に変更されました')
     console.log(storage)
   })
 }
 // 設定更新
 document.getElementById('save')?.addEventListener('click', ()=>{
-  chrome.storage.local.clear()
+  chrome.storage.local.set({ kw: {} })
+  const newKw: { [key: string]: string } = {}
   const numberOfInputs: number = document.getElementsByTagName('input').length / 2
   for (let i = 0; i < numberOfInputs - 1; i++) {
-    const newK = <HTMLInputElement>document.getElementById('k-' + i)
+    const newK = document.getElementById('k-' + i) as HTMLInputElement
     let newKey = newK.value.trim()
-    const newV = <HTMLInputElement>document.getElementById('v-' + i)
+    const newV = document.getElementById('v-' + i)  as HTMLInputElement
     let newVal = newV.value
-    if(newKey && newKey !== 'extensionFunction') {
-      chrome.storage.local.set({ [newKey]: newVal })
+    if(newKey.length > 0) {
+      newKw[newKey] = newVal
     }
   }
-  location.reload()
+  chrome.storage.local.set({ kw: newKw })
+  notice('設定が保存されました。')
+  // chrome.storage.local.get(null, (storage)=>{
+  //   console.log('設定完了')
+  //   console.log(storage)
+  // })
+  // location.reload()
 })
 
 //<input> 作成 & 追加
@@ -63,7 +65,7 @@ const addInput = (parent: HTMLElement, num: number, key: string = '', val: strin
   const $div = document.createElement('div')
   $div.className = 'input-pair'
   parent.appendChild($div)
-  const $newInputKey = <HTMLInputElement>document.createElement('input')
+  const $newInputKey = document.createElement('input')
   $newInputKey.type = 'search'
   $newInputKey.id = 'k-' + num
   $newInputKey.className = 'input'
@@ -80,4 +82,16 @@ const addInput = (parent: HTMLElement, num: number, key: string = '', val: strin
   $div.appendChild($newInputVal)
 }
 
-const initialSettings = { 'コロナ': 'コーラ', 'マスク': 'フリスク' }
+const initialSettings = {
+  affect: true,
+  kw: {
+    'コロナ': 'コーラ',
+    'マスク': 'フリスク'
+  }
+}
+
+const notice = (message: string) => {
+  const $notice = document.getElementById('notice') as HTMLElement
+  $notice.textContent = message
+}
+  // chrome.storage.local.clear()
